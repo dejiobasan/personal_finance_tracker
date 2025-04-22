84a78e3f205c185bd0a93c3e558a9b6cdb2d7c7e
dejiobasan <dejiobasan@yahoo.com>
2025-04-22 18:30:29 +0100

    Add Firebase configuration files and update project settings

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:personal_finance_tracker/screens/home_screen.dart';
+import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MaterialApp(title: "Material App", home: HomeScreen()));
 }

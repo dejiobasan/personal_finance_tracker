e02c1e147abf024804e35894835577525fee450f
dejiobasan <dejiobasan@yahoo.com>
2025-04-22 12:31:30 +0100

    Add Windows runner files for Flutter application
    - Created runner executable manifest to support DPI awareness and compatibility with Windows 10 and 11.
    - Implemented utility functions for console creation, command line argument retrieval, and UTF-16 to UTF-8 string conversion in utils.cpp and utils.h.
    - Developed Win32Window class for managing a high DPI-aware window, including message handling, theme updates, and child content management in win32_window.cpp and win32_window.h.

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:personal_finance_tracker/screens/home_screen.dart';
+
+void main() {
+  runApp(MaterialApp(title: "Material App", home: HomeScreen()));
+}
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  static const Color customBlue950 = Color(0xFF0D47A1);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.blue[500],
+              image: DecorationImage(
+                image: AssetImage(
+                  'assets/images/Shape.png',
+                ),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Positioned(
+            top: 20,
+            left: 20,
+            child: Icon(
+              Icons.account_balance_wallet,
+              size: 40,
+              color: Colors.white,
+            ),
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Column(
+                  children: [
+                    Text(
+                      'Manage your Transactions Effortlessly',
+                      style: TextStyle(
+                        fontSize: 60,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      'The ultimate transaction management platform to safeguard your finances '
+                      'and ensure you never miss a payment.',
+                      style: TextStyle(fontSize: 25, color: Colors.white),
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor:
+                            customBlue950,
+                        foregroundColor:
+                            Colors
+                                .white,
+                      ),
+                      onPressed: () {
+                        
+                      },
+                      child: Text('Register'),
+                    ),
+                    SizedBox(width: 20),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor:
+                            customBlue950,
+                        foregroundColor:
+                            Colors
+                                .white,
+                      ),
+                      onPressed: () {
+
+                      },
+                      child: Text('Login'),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
